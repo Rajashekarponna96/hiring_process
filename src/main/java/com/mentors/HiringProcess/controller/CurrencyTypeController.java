0ef7e7c51648c489bb9d732025ee0439bfc11ed2
shaliniEdara <edara.shalini@gmail.com>
2024-03-06 13:16:14 +0530

    added currency controller api

--- src/main/java/com/mentors/HiringProcess/controller/CurrencyTypeController.java
+++ src/main/java/com/mentors/HiringProcess/controller/CurrencyTypeController.java
@@ -1,29 +1,33 @@
 package com.mentors.HiringProcess.controller;
 
+
+
+
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mentors.HiringProcess.dto.SourceDto;
-import com.mentors.HiringProcess.service.SourceService;
+import com.mentors.HiringProcess.dto.CurrencyTypeDto;
+import com.mentors.HiringProcess.model.CurrencyType;
+import com.mentors.HiringProcess.service.CurrencyTypeService;
 
 @RestController
-@RequestMapping(value="/source")
+@RequestMapping(value="/currency")
 @CrossOrigin
-public class SourceController {
+public class CurrencyTypeController {
 	@Autowired
-	private SourceService sourceService;
+	CurrencyTypeService currencyTypeService;
 	
-	
-	@GetMapping(value="/all")
-	public List<SourceDto> getAll(){
-		return sourceService.getAll();
+	@GetMapping(value="/all")  
+	public List<CurrencyTypeDto> getAllCurrencyList(){
+		return currencyTypeService.getAll();
 	}
+	
 	
 
 }
---
+++ src/main/java/com/mentors/HiringProcess/service/CurrencyTypeService.java
@@ -0,0 +1,11 @@
+package com.mentors.HiringProcess.service;
+
+import java.util.List;
+
+import com.mentors.HiringProcess.dto.CurrencyTypeDto;
+
+public interface CurrencyTypeService {
+	
+	 public List<CurrencyTypeDto> getAll();
+
+}
