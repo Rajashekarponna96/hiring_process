e33f91d09f7114b852abf72635a54ac1b7d69f2f
shaliniEdara <edara.shalini@gmail.com>
2024-05-03 15:03:16 +0530

    added emaillistwithpagination api in emailtemplate controller

--- src/main/java/com/mentors/HiringProcess/service/EmailTemplateServiceI.java
+++ src/main/java/com/mentors/HiringProcess/service/EmailTemplateServiceI.java
@@ -2,6 +2,10 @@ package com.mentors.HiringProcess.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.mentors.HiringProcess.dto.CandidateDto;
 import com.mentors.HiringProcess.dto.EmailTemplateDto;
 
 public interface EmailTemplateServiceI {
@@ -15,6 +19,8 @@ public interface EmailTemplateServiceI {
 	void delete(Long id);
 
 	EmailTemplateDto getSubjectAndBodyByTitle(String title);
+	
+	Page<EmailTemplateDto> getAllEmailsWithPagination(Pageable pageable);
 
 	
 
