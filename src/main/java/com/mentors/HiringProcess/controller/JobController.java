e7d7214ff43ba805e5d4138d38c2f83b6ed059a2
rajashekarponna96 <rajashekar.ponna96@gmail.com>
2024-05-24 11:18:11 +0530

    assingjoobstovendorapis

--- src/main/java/com/mentors/HiringProcess/controller/JobController.java
+++ src/main/java/com/mentors/HiringProcess/controller/JobController.java
@@ -1,11 +1,13 @@
 package com.mentors.HiringProcess.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mentors.HiringProcess.dto.ClientDto;
 import com.mentors.HiringProcess.dto.JobDto;
 import com.mentors.HiringProcess.dto.JobSummaryDto;
+import com.mentors.HiringProcess.model.Job;
+import com.mentors.HiringProcess.model.Vendor;
 import com.mentors.HiringProcess.service.JobServiceI;
-
-
 
 @RestController
 @RequestMapping(value = "/job")
@@ -31,55 +32,67 @@ public class JobController {
 
 	@Autowired
 	private JobServiceI jobServiceI;
-	
+
 	@PostMapping("/")
-	public void addJob(@RequestBody JobDto  jobDto) {
+	public void addJob(@RequestBody JobDto jobDto) {
 		jobDto.validateRequiredAttibutes(jobDto);
 		jobServiceI.addJob(jobDto);
 	}
-	
+
 	@GetMapping("/all")
 	public List<JobDto> allJobs() {
 		return jobServiceI.allJobs();
 	}
-	
-	
+
 	@PutMapping("/{id}")
-	public void updateJob(@PathVariable Long id,  @RequestBody JobDto  jobDto) {
-		jobServiceI.updateJob(id,jobDto);
+	public void updateJob(@PathVariable Long id, @RequestBody JobDto jobDto) {
+		jobServiceI.updateJob(id, jobDto);
 	}
-	
-	
+
 	@DeleteMapping("/{id}")
 	public void deleteJob(@PathVariable Long id) {
 		jobServiceI.deleteJob(id);
 	}
-	
+
 	@GetMapping("/summaries")
-    public List<JobSummaryDto> getAllJobSummaries() {
-        return jobServiceI.getAllJobSummaries();
-    }
+	public List<JobSummaryDto> getAllJobSummaries() {
+		return jobServiceI.getAllJobSummaries();
+	}
 
-	
-	 @GetMapping("/with-clients")
-	    public List<JobDto> getAllJobsWithClients() {
-	        return jobServiceI.allJobsWithClients();
-	    }
-	 
+	@GetMapping("/with-clients")
+	public List<JobDto> getAllJobsWithClients() {
+		return jobServiceI.allJobsWithClients();
+	}
 
-	 @GetMapping("/searchpage")
-     public Page<JobDto> getAllJobs(@RequestParam int page, @RequestParam int size,
-		        @RequestParam String code) {
-		    Pageable pageable = PageRequest.of(page, size);
-		    return jobServiceI.getAllJobs(pageable,code);
-		}
-	 
-	 
-	 @GetMapping("/joblistwithpagination")
-     public Page<JobDto> getAllJobsWithPagination(@RequestParam int page, @RequestParam int size) {
-		    Pageable pageable = PageRequest.of(page, size);
-		    return jobServiceI.getAllJobsWithPagination(pageable);
-		}
-	 
-	 
+	@GetMapping("/searchpage")
+	public Page<JobDto> getAllJobs(@RequestParam int page, @RequestParam int size, @RequestParam String code) {
+		Pageable pageable = PageRequest.of(page, size);
+		return jobServiceI.getAllJobs(pageable, code);
+	}
+
+	@GetMapping("/joblistwithpagination")
+	public Page<JobDto> getAllJobsWithPagination(@RequestParam int page, @RequestParam int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		return jobServiceI.getAllJobsWithPagination(pageable);
+	}
+
+	//
+	@GetMapping("/alljobslist")
+	public List<Job> getAllJobs() {
+		return jobServiceI.getAllJobs();
+	}
+
+	@GetMapping("/allvendorslist")
+	public List<Vendor> getAllVendors() {
+		return jobServiceI.getAllVendors();
+	}
+ 
+	@PostMapping("/{jobId}/vendors")
+	public ResponseEntity<Void> assignVendorToJob(@PathVariable Long jobId,
+			@RequestBody Map<String, Long> requestBody) {
+		Long vendorId = requestBody.get("vendorId");
+		jobServiceI.assignVendorToJob(jobId, vendorId);
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/com/mentors/HiringProcess/service/JobServiceImpl.java
+++ src/main/java/com/mentors/HiringProcess/service/JobServiceImpl.java
@@ -18,8 +18,9 @@ import com.mentors.HiringProcess.dto.JobDto;
 import com.mentors.HiringProcess.dto.JobSummaryDto;
 import com.mentors.HiringProcess.model.Client;
 import com.mentors.HiringProcess.model.Job;
+import com.mentors.HiringProcess.model.Vendor;
 import com.mentors.HiringProcess.repository.JobRepository;
-import com.mentors.HiringProcess.specification.ClientSprecifications;
+import com.mentors.HiringProcess.repository.VendorRepository;
 import com.mentors.HiringProcess.specification.JobSpecifications;
 
 @Service
@@ -28,6 +29,10 @@ public class JobServiceImpl implements JobServiceI {
 
 	@Autowired
 	private JobRepository jobRepository;
+
+	//
+	@Autowired
+	private VendorRepository vendorRepository;
 
 	@Autowired
 	private JobBuilder jobBuilder;
@@ -49,18 +54,18 @@ public class JobServiceImpl implements JobServiceI {
 		}
 		return jobsList;
 	}
-	
-	 @Override
-	    public List<JobSummaryDto> getAllJobSummaries() {
-	        List<JobSummaryDto> jobSummaries = new ArrayList<>();
-	        List<Job> jobs = jobRepository.findAll();
-	        if (jobs != null) {
-	            jobs.forEach(job -> {
-	                jobSummaries.add(new JobSummaryDto(job.getTitle(), job.getOpenings()));
-	            });
-	        }
-	        return jobSummaries;
-	    }
+
+	@Override
+	public List<JobSummaryDto> getAllJobSummaries() {
+		List<JobSummaryDto> jobSummaries = new ArrayList<>();
+		List<Job> jobs = jobRepository.findAll();
+		if (jobs != null) {
+			jobs.forEach(job -> {
+				jobSummaries.add(new JobSummaryDto(job.getTitle(), job.getOpenings()));
+			});
+		}
+		return jobSummaries;
+	}
 
 	@Override
 	public void updateJob(Long id, JobDto jobDto) {
@@ -77,58 +82,78 @@ public class JobServiceImpl implements JobServiceI {
 		jobRepository.deleteById(id);
 	}
 
-	
 	@Override
-    public List<JobDto> allJobsWithClients() {
-        List<Job> jobs = jobRepository.findAll();
-        return jobs.stream()
-                   .map(this::mapJobToJobDtoWithClient)
-                   .collect(Collectors.toList());
-    }
+	public List<JobDto> allJobsWithClients() {
+		List<Job> jobs = jobRepository.findAll();
+		return jobs.stream().map(this::mapJobToJobDtoWithClient).collect(Collectors.toList());
+	}
 
-    private JobDto mapJobToJobDtoWithClient(Job job) {
-        JobDto jobDto = new JobDto();
-        jobDto=  jobBuilder.toDto(job);
-        // Map job attributes
+	private JobDto mapJobToJobDtoWithClient(Job job) {
+		JobDto jobDto = new JobDto();
+		jobDto = jobBuilder.toDto(job);
+		// Map job attributes
 //        jobDto.setId(job.getId());
 //        jobDto.setTitle(job.getTitle());
 //        jobDto.setDescription(job.getDescription());
 //        jobDto.setOpenings(job.getOpenings());
 //        
-        // Map other job attributes...
+		// Map other job attributes...
 
-        // Fetch client for the job
-        Client client = job.getClients(); // Assuming there's a method to get the client associated with the job
-        if (client != null) {
-            ClientDto clientDto = new ClientDto();
-            clientDto.setId(client.getId());
-            clientDto.setCompanyName(client.getCompanyName());
-            // Map other client attributes...
+		// Fetch client for the job
+		Client client = job.getClients(); // Assuming there's a method to get the client associated with the job
+		if (client != null) {
+			ClientDto clientDto = new ClientDto();
+			clientDto.setId(client.getId());
+			clientDto.setCompanyName(client.getCompanyName());
+			// Map other client attributes...
 
-            jobDto.setClients(clientDto);
-        }
+			jobDto.setClients(clientDto);
+		}
 
-        return jobDto;
-    }
+		return jobDto;
+	}
 
 	@Override
 	public Page<JobDto> getAllJobs(Pageable pageable, String code) {
 		Specification<Job> spec = Specification.where(null); // Start with an empty specification
 
-        if (code != null && !code.isEmpty()) {
-            spec = spec.and(JobSpecifications.hasFields(code));
-        }
+		if (code != null && !code.isEmpty()) {
+			spec = spec.and(JobSpecifications.hasFields(code));
+		}
 
-        Page<Job> recruiterPage = jobRepository.findAll(spec, pageable);
-        return recruiterPage.map(jobBuilder::toDto);
-		
+		Page<Job> recruiterPage = jobRepository.findAll(spec, pageable);
+		return recruiterPage.map(jobBuilder::toDto);
+
 	}
 
 	@Override
 	public Page<JobDto> getAllJobsWithPagination(Pageable pageable) {
-		Page<Job> jobPage =jobRepository.findAll(pageable);
+		Page<Job> jobPage = jobRepository.findAll(pageable);
 		return jobPage.map(jobBuilder::toDto);
-		
+
 	}
-		
+
+	//
+	@Override
+	public List<Job> getAllJobs() {
+		return jobRepository.findAll();
+	}
+
+	@Override
+	public List<Vendor> getAllVendors() {
+		return vendorRepository.findAll();
+	}
+
+	@Override
+	public void assignVendorToJob(Long jobId, Long vendorId) {
+		if (jobId == null || vendorId == null) {
+			throw new IllegalArgumentException("Job ID and Vendor ID must not be null");
+		}
+
+		Job job = jobRepository.findById(jobId).orElseThrow(() -> new RuntimeException("Job not found"));
+		Vendor vendor = vendorRepository.findById(vendorId).orElseThrow(() -> new RuntimeException("Vendor not found"));
+
+		job.setVendor(vendor);
+		jobRepository.save(job);
+	}
 }
