41629763462809e86ba0b2261ff5745de8e561aa
shaliniEdara <edara.shalini@gmail.com>
2024-05-27 10:40:40 +0530

    shedular code final changes mail send for delay process based on candidate table

--- src/main/java/com/mentors/HiringProcess/repository/HiringFlowActivityRepository.java
+++ src/main/java/com/mentors/HiringProcess/repository/HiringFlowActivityRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.mentors.HiringProcess.model.HiringFlowActivity;
+import com.mentors.HiringProcess.model.HiringFlowType;
 
 public interface HiringFlowActivityRepository extends JpaRepository<HiringFlowActivity, Long>{
 	 List<HiringFlowActivity> findByCandidateId(Long candidateId);
 	 
-	 @Query("SELECT c FROM HiringFlowActivity c WHERE c.hiringFlowType = 1")
-	 List<HiringFlowActivity> findByHiringFlowType();
+	 @Query("SELECT c FROM HiringFlowActivity c WHERE c.hiringFlowType = hiringFlowType")
+	 List<HiringFlowActivity> findByHiringFlowType( HiringFlowType  hiringFlowType);
 
 }
--- src/main/java/com/mentors/HiringProcess/service/MailServiceImpl.java
+++ src/main/java/com/mentors/HiringProcess/service/MailServiceImpl.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 import com.mentors.HiringProcess.model.Candidate;
 import com.mentors.HiringProcess.model.EmailTemplate;
 import com.mentors.HiringProcess.model.HiringFlowActivity;
+import com.mentors.HiringProcess.model.HiringFlowType;
 import com.mentors.HiringProcess.model.Job;
 import com.mentors.HiringProcess.model.Recruiter;
 import com.mentors.HiringProcess.repository.CandidateRepository;
@@ -22,6 +24,11 @@ import com.mentors.HiringProcess.repository.JobRepository;
 
 @Component
 public class MailServiceImpl {
+	
+	private static Logger log = Logger.getAnonymousLogger();
+	
+	//private static Logger logger = Logger.getLogger('hello', MailServiceImpl.class)
+	
 	@Autowired
 	public CandidateRepository candidateRepository;
 	
@@ -37,17 +44,29 @@ public class MailServiceImpl {
 	@Autowired
 	public HiringFlowActivityRepository hiringFlowActivityRepository;
 	
+	
+	
 	@Scheduled(fixedRate=6000)
 	public void mailSend() {
 		
-		//sourcedDelayTime();
-		screenincreeningDEelayTime();
+		sourcedDelayTime();
+		screenincreeningDelayTime();
+		InterviewDelayTime();
+		PreboardingDelayTime();
+		HiredDelayTime();
+		ArchivedDelayTime();
+		HoldDelayTime();
+		
 		
 	}
 		
 		public void sourcedDelayTime(){
-			List<Candidate> candidateList = candidateRepository.findByStage();
-		System.out.println("we are in sourced delay time ----------------------");
+			HiringFlowType  hiringFlowType=  HiringFlowType.Sourced;
+//	        List<HiringFlowActivity> hiringFlowActivityList = hiringFlowActivityRepository.findByHiringFlowType(hiringFlowType);
+//	        sendMailsForDelayProcess(hiringFlowActivityList,hiringFlowType);
+//			
+			List<Candidate> candidateList = candidateRepository.findByStage(hiringFlowType);
+		
 		LocalDateTime   presentTime = LocalDateTime.now();
 		for (Candidate candidate : candidateList) {
 			if(candidate!=null) {
@@ -85,23 +104,83 @@ public class MailServiceImpl {
 			
 		}
 		
-	public void screenincreeningDEelayTime(){
-			
-		List<HiringFlowActivity> hiringFlowActivityList = hiringFlowActivityRepository.findByHiringFlowType();
-		System.out.println("we are in screening delay time ----------------------");
+	public void screenincreeningDelayTime(){
+		HiringFlowType  hiringFlowType=  HiringFlowType.Screening;
+//        List<HiringFlowActivity> hiringFlowActivityList = hiringFlowActivityRepository.findByHiringFlowType(hiringFlowType);
+//        sendMailsForDelayProcess(hiringFlowActivityList,hiringFlowType);
+		
+		List<Candidate> candidateList = candidateRepository.findByStage(hiringFlowType);
+		sendMailsToRecrutiterForDelayProcess(candidateList);
+		
+		}
+	
+	public void InterviewDelayTime() {
+		HiringFlowType  hiringFlowType=  HiringFlowType.Interview;
+//        List<HiringFlowActivity> hiringFlowActivityList = hiringFlowActivityRepository.findByHiringFlowType(hiringFlowType);
+//        sendMailsForDelayProcess(hiringFlowActivityList,hiringFlowType);
+        
+        
+        List<Candidate> candidateList = candidateRepository.findByStage(hiringFlowType);
+		sendMailsToRecrutiterForDelayProcess(candidateList);
+	}
+	
+	public void PreboardingDelayTime(){
+		HiringFlowType  hiringFlowType=  HiringFlowType.Preboarding;
+//        List<HiringFlowActivity> hiringFlowActivityList = hiringFlowActivityRepository.findByHiringFlowType(hiringFlowType);
+//        sendMailsForDelayProcess(hiringFlowActivityList,hiringFlowType);
+		
+		List<Candidate> candidateList = candidateRepository.findByStage(hiringFlowType);
+		sendMailsToRecrutiterForDelayProcess(candidateList);
+		
+	}
+	
+	public void HiredDelayTime() {
+		HiringFlowType  hiringFlowType=  HiringFlowType.Screening;
+//        List<HiringFlowActivity> hiringFlowActivityList = hiringFlowActivityRepository.findByHiringFlowType(hiringFlowType);
+//        sendMailsForDelayProcess(hiringFlowActivityList,hiringFlowType);
+		
+		List<Candidate> candidateList = candidateRepository.findByStage(hiringFlowType);
+		sendMailsToRecrutiterForDelayProcess(candidateList);
+		
+	}
+	
+	public void  ArchivedDelayTime() {
+		HiringFlowType  hiringFlowType=  HiringFlowType.Archived;
+//        List<HiringFlowActivity> hiringFlowActivityList = hiringFlowActivityRepository.findByHiringFlowType(hiringFlowType);
+//        sendMailsForDelayProcess(hiringFlowActivityList,hiringFlowType);
+//		
+		List<Candidate> candidateList = candidateRepository.findByStage(hiringFlowType);
+		sendMailsToRecrutiterForDelayProcess(candidateList);
+	}
+	
+	
+	
+	public void HoldDelayTime() {
+		HiringFlowType  hiringFlowType=  HiringFlowType.Hold;
+//        List<HiringFlowActivity> hiringFlowActivityList = hiringFlowActivityRepository.findByHiringFlowType(hiringFlowType);
+//        sendMailsForDelayProcess(hiringFlowActivityList,hiringFlowType);
+		
+		List<Candidate> candidateList = candidateRepository.findByStage(hiringFlowType);
+		sendMailsToRecrutiterForDelayProcess(candidateList);
+		
+	}
+	
+	//send mails based on hiring filow actitity table
+	public void  sendMailsForDelayProcess(List<HiringFlowActivity> hiringFlowActivityList,HiringFlowType  hiringFlowType) {
 		LocalDateTime   presentTime = LocalDateTime.now();
 		for (HiringFlowActivity hiringFlowActivity : hiringFlowActivityList) {
 			LocalDateTime dbTime = hiringFlowActivity.getCreatedDate();
 			Duration duration = Duration.between(dbTime, presentTime);
 	        long timedifference = duration.toMinutes(); // You can change toSeconds(), toHours(), etc. based on your need
-	        System.out.println("Time difference in minutes: " + timedifference);
+	        //System.out.println("Time difference in minutes: " + timedifference);
+	        log.info("Time difference in minutes: " + timedifference);
 	        long fixedTime = 2880;
 	        if(timedifference > 2880) {
 	        	Recruiter recruiter = hiringFlowActivity.getCandidate().getJob().getRecruiters();
 	        	System.out.println("recruter details are------"+recruiter.getEmail());
 	        	String recruiterName = recruiter.getFirstName();
 	        	String stage = hiringFlowActivity.getCandidate().getStage().toString();
-	        	EmailTemplate emailTemplate  = emailTemplateRepository.findByTitle("Screening");
+	        	EmailTemplate emailTemplate  = emailTemplateRepository.findByTitle(hiringFlowActivity.getHiringFlowType().name());
 	        	if(emailTemplate != null) {
 	    		String body=emailTemplate.getBody();
 	    		String uploadDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
@@ -115,8 +194,47 @@ public class MailServiceImpl {
 			
 		}
 		
+	}
+	
+	
+	//send mails for delay process based on candidate table
+	
+	public void sendMailsToRecrutiterForDelayProcess(List<Candidate> candidateList) {
+		LocalDateTime   presentTime = LocalDateTime.now();
+		for (Candidate candidate : candidateList) {
+			if(candidate!=null) {
+			Long candidateId = candidate.getId();
+			 Job jobDetails = candidate.getJob();
+			 if(jobDetails!=null) {
+				 Recruiter recruiterDetails =jobDetails.getRecruiters();
+				 
+			LocalDateTime	createdTime =candidate.getModifiedTimestamp();
+			Duration duration = Duration.between(createdTime, presentTime);
+	        long timedifference = duration.toMinutes(); // You can change toSeconds(), toHours(), etc. based on your need
+	        System.out.println("Time difference in minutes: " + timedifference);
+	        long fixedTime = 2880;
+	        if(timedifference > 2880) {
+	        	
+	        	String uploadDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+	        	EmailTemplate emailTemplate  = emailTemplateRepository.findByTitle(candidate.getStage().toString());
+	    		String body=emailTemplate.getBody();
+	    		String recruiterName = recruiterDetails.getFirstName();
+	    		String updatedBodyForRecruiter = body.replace("[CandidateName]", recruiterName)
+	                     .replace("[UploadDate]", uploadDate);
+	    		emailService.sendSimpleMessage(recruiterDetails.getEmail(), emailTemplate.getSubject(),updatedBodyForRecruiter,null, null, null);
+
+
+	        }
+			
+			 }
 			
 		}
+		}
+         
+		
+	}
+	
+	
 		
 		
 	
